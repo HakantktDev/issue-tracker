2d821578127052c1da5c6188715fd51eafdb7832
HakantktDev <hakantokatdev@gmail.com>
2025-12-09 08:36:41 +0300

    issues added

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -12,6 +12,7 @@ export default async function DashboardLayout({
       <Navigation />
       <main className="pl-16 md:pl-64 pt-0 min-h-screen">
         <div className="max-w-6xl mx-auto p-4 md:p-8">
+          {/* loading component takes the place */}
           {/* <Suspense fallback={<DashboardSkeleton />}>{children}</Suspense> */}
           {children}
         </div>
